0f0e30f10dc229cd1e87bf0c5d4fe45d22560c36
NPY4D <ninjapadi22@gmail.com>
2026-07-12 22:40:20 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -346,4 +346,4 @@ function doReveal(room) {
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`QuizBan :${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`QuizBan :${PORT}`));
